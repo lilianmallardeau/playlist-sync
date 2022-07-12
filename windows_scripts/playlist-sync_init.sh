a3ebe9cda0d05bd993cde33b64f20283d6636d0d
Christian Michiels <101425164+crjmichiels@users.noreply.github.com>
2022-07-12 10:52:36 -0400

    Delete windows_scripts directory

--- windows_scripts/playlist-sync_init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-@echo off
-playlist-sync init
-echo Done! Now, fill the config.json and playlists.json files.
-pause
--- windows_scripts/playlist-sync_sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-@echo off
-playlist-sync sync
-echo.
-echo Done!
-pause
